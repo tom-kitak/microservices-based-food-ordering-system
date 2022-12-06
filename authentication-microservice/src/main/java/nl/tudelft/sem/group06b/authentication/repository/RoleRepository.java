59fb6779959002fb81e7d0b1c4392f03b301c5b1
Catalin Brita <c.e.brita@student.tudelft.nl>
2022-12-06 13:47:20 +0100

    Added javadoc to repositories

--- authentication-microservice/src/main/java/nl/tudelft/sem/group06b/authentication/repository/RoleRepository.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/group06b/authentication/repository/RoleRepository.java
@@ -3,6 +3,9 @@ package nl.tudelft.sem.group06b.authentication.repository;
 import nl.tudelft.sem.group06b.authentication.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Stores the different roles that a user can have in the system.
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
