c48dedcf15859abb6ba01caeeb446660ca0cc32c
Yinzheng Qu <Y.Qu@student.tudelft.nl>
2022-12-08 23:08:23 +0100

    Fixing checkstyle

--- store-microservice/src/main/java/nl/tudelft/sem/group06b/store/domain/Store.java
+++ store-microservice/src/main/java/nl/tudelft/sem/group06b/store/domain/Store.java
@@ -1,9 +1,12 @@
 package nl.tudelft.sem.group06b.store.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.NoArgsConstructor;
 
 
