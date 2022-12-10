eb3574d4e5877216da2c60fbe45e872b7d1d0945
Kevin Hoxha <K.Hoxha@student.tudelft.nl>
2022-12-10 22:12:37 +0100

    Adding endpoints for adding and deleting allergies

--- user-microservice/src/main/java/nl/tudelft/sem/group06b/user/controllers/UserController.java
+++ user-microservice/src/main/java/nl/tudelft/sem/group06b/user/controllers/UserController.java
@@ -3,14 +3,16 @@ package nl.tudelft.sem.group06b.user.controllers;
 import java.util.List;
 import nl.tudelft.sem.group06b.user.authentication.AuthManager;
 import nl.tudelft.sem.group06b.user.domain.Allergy;
-import nl.tudelft.sem.group06b.user.domain.Role;
+import nl.tudelft.sem.group06b.user.domain.User;
 import nl.tudelft.sem.group06b.user.domain.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -38,10 +40,59 @@ public class UserController {
      *
      * @return the list of allergens found in the database with the given memberId
      */
-    @GetMapping("/user/{memberId}/allergens")
+    @GetMapping("/user/{memberId}/getAllergens")
     public ResponseEntity<List<Allergy>> getAllergens(@PathVariable String memberId) throws Exception {
         return ResponseEntity.ok(userService.getUser(memberId).getAllergies());
     }
+
+    /**
+     * Gets allergens by memberId.
+     *
+     * @return the list of allergens found in the database with the given memberId
+     */
+    @PutMapping("/user/{memberId}/{allergen}/addAllergen")
+    public ResponseEntity<List<Allergy>> addAllergens(@PathVariable String memberId,
+                                                      @PathVariable String allergen) throws Exception {
+        Allergy allergy = new Allergy(allergen);
+        User user = userService.getUser(memberId);
+        List<Allergy> userAllergies = user.getAllergies();
+        if (userAllergies.contains(allergy)) {
+            return ResponseEntity.badRequest().build();
+        }
+        try {
+            userAllergies.add(allergy);
+            userService.addUser(authManager.getMemberId(), authManager.getRole(),
+                    userAllergies, user.getPreferredLocation());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return ResponseEntity.ok(userService.getUser(memberId).getAllergies());
+    }
+
+    /**
+     * Gets allergens by memberId.
+     *
+     * @return the list of allergens found in the database with the given memberId
+     */
+    @DeleteMapping("/user/{memberId}/removeAllergen")
+    public ResponseEntity<List<Allergy>> removeAllergens(@PathVariable String memberId,
+                                                         @PathVariable String allergen) throws Exception {
+        Allergy allergy = new Allergy(allergen);
+        User user = userService.getUser(memberId);
+        List<Allergy> userAllergies = user.getAllergies();
+        if (!userAllergies.contains(allergy)) {
+            return ResponseEntity.badRequest().build();
+        }
+        try {
+            userAllergies.remove(allergy);
+            userService.addUser(authManager.getMemberId(), authManager.getRole(),
+                    userAllergies, user.getPreferredLocation());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return ResponseEntity.ok(userService.getUser(memberId).getAllergies());
+    }
+
 
     /**
      * Adds user to the database.
@@ -51,7 +102,7 @@ public class UserController {
     @PostMapping("/register/user")
     public ResponseEntity registerUser() throws Exception {
         try {
-            userService.addUser(authManager.getMemberId(), Role.USER_CUSTOMER, null, null);
+            userService.addUser(authManager.getMemberId(), authManager.getRole(), null, null);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
