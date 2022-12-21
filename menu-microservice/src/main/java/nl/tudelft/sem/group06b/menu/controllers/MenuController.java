639b7f3b57735703eb1b556a98ed1c00a9b2b405
Kevin Hoxha <K.Hoxha@student.tudelft.nl>
2022-12-21 15:34:40 +0100

    Querying user allergens

--- menu-microservice/src/main/java/nl/tudelft/sem/group06b/menu/controllers/MenuController.java
+++ menu-microservice/src/main/java/nl/tudelft/sem/group06b/menu/controllers/MenuController.java
@@ -3,19 +3,25 @@ package nl.tudelft.sem.group06b.menu.controllers;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import nl.tudelft.sem.group06b.menu.authentication.AuthManager;
 import nl.tudelft.sem.group06b.menu.domain.Allergy;
 import nl.tudelft.sem.group06b.menu.domain.MenuService;
 import nl.tudelft.sem.group06b.menu.domain.Pizza;
 import nl.tudelft.sem.group06b.menu.domain.Topping;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
@@ -78,7 +84,19 @@ public class MenuController {
      */
     @GetMapping("getAllPizzas")
     public ResponseEntity<List<Pizza>> getAllPizzas() {
-        return ResponseEntity.ok(menuService.getAllPizzas());
+        String url = "http://localhost:8082/user/" + authManager.getMemberId() + "/getAllergens";
+
+        final RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", String.format("Bearer %s", authManager.getToken()));
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<List> response = restTemplate.exchange(url, HttpMethod.GET, entity, List.class);
+        List<String> allergens = (List<String>) response.getBody().stream()
+                .map(x -> x.toString().split("=")[1].split("}")[0]).collect(Collectors.toList());
+
+        return ResponseEntity.ok(menuService.filterPizzasByAllergens(allergens));
     }
 
     /**
