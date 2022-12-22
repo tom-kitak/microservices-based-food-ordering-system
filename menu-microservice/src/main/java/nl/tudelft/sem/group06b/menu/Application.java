77af3235562b5b649b1c0ef246508398c15a265a
Ishan Pahwa <I.S.Pahwa@student.tudelft.nl>
2022-12-22 05:59:01 +0100

    Security commits

--- menu-microservice/src/main/java/nl/tudelft/sem/group06b/menu/Application.java
+++ menu-microservice/src/main/java/nl/tudelft/sem/group06b/menu/Application.java
@@ -1,13 +1,7 @@
 package nl.tudelft.sem.group06b.menu;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-/**
- * Example microservice application.
- */
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
