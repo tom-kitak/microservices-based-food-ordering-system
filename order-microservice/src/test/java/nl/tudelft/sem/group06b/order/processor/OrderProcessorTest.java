790adbb1a09aa29591dddbe3f5f78463b0875b6a
MarkArdman <94811276+MarkArdman@users.noreply.github.com>
2022-12-23 20:34:59 +0100

    Finalized testing of certain functions in OrderProcessorImpl, testing order cancellation

--- order-microservice/src/test/java/nl/tudelft/sem/group06b/order/processor/OrderProcessorTest.java
+++ order-microservice/src/test/java/nl/tudelft/sem/group06b/order/processor/OrderProcessorTest.java
@@ -1,13 +1,19 @@
 package nl.tudelft.sem.group06b.order.processor;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import nl.tudelft.sem.group06b.order.communication.CouponCommunication;
 import nl.tudelft.sem.group06b.order.communication.MenuCommunication;
@@ -17,6 +23,7 @@ import nl.tudelft.sem.group06b.order.domain.Pizza;
 import nl.tudelft.sem.group06b.order.domain.Status;
 import nl.tudelft.sem.group06b.order.repository.OrderRepository;
 import nl.tudelft.sem.group06b.order.service.processor.OrderProcessor;
+import nl.tudelft.sem.group06b.order.service.processor.OrderProcessorImpl;
 import nl.tudelft.sem.group06b.order.util.TimeValidation;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +53,28 @@ public class OrderProcessorTest {
 
     @Autowired
     private OrderProcessor orderProcessor;
+
+    @Test
+    public void testConstruction() {
+        Order order1 = new Order();
+        order1.setStatus(Status.ORDER_PLACED);
+        order1.setSelectedTime("11/11/2029 11:11:11");
+        order1.setId(1L);
+        Order order2 = new Order();
+        order2.setStatus(Status.ORDER_FINISHED);
+        order2.setSelectedTime("11/11/2019 11:11:11");
+        order2.setId(2L);
+        List<Order> orders = List.of(order1, order2);
+        when(mockOrderRepository.findAll()).thenReturn(orders);
+        when(mockOrderRepository.getOne(1L)).thenReturn(order1);
+
+        OrderProcessor testOrderProcessor = new OrderProcessorImpl(
+                mockOrderRepository, mockTaskScheduler, mockMenuCommunication,
+                mockStoreCommunication, mockCouponCommunication, mockTimeValidation);
+
+        verify(mockOrderRepository, times(1)).getOne(1L);
+        verify(mockTaskScheduler, times(1)).schedule(any(), (Date) any());
+    }
 
     @Test
     public void testStartOrderCorrect() throws Exception {
@@ -93,12 +122,22 @@ public class OrderProcessorTest {
     }
 
     @Test
-    public void testSetOrderTimeInvalidTime() throws Exception {
-        Order order = new Order();
+    public void testSetOrderTimeInvalidTime() {
         Long orderId = 1L;
         assertThrows(
                 IllegalArgumentException.class,
                 () -> orderProcessor.setOrderTime(orderId, ""),
+                "Invalid time. The time should be at least 30 minutes after the current time");
+        verify(mockOrderRepository, never()).save(any(Order.class));
+    }
+
+    @Test
+    public void testSetOrderTimeBefore() throws Exception {
+        Long orderId = 1L;
+        when(mockTimeValidation.isTimeValid("11", 30)).thenReturn(false);
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> orderProcessor.setOrderTime(orderId, "11"),
                 "Invalid time. The time should be at least 30 minutes after the current time");
         verify(mockOrderRepository, never()).save(any(Order.class));
     }
@@ -145,6 +184,15 @@ public class OrderProcessorTest {
         targetOrder.setStoreId(storeId);
 
         verify(mockOrderRepository, times(1)).save(any(Order.class));
+    }
+
+    @Test
+    public void testSetOrderLocationEmptyToken() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> orderProcessor.setOrderLocation("", 1L, "Drebbelweg 40"),
+                "Invalid token");
+        verify(mockOrderRepository, never()).save(any(Order.class));
     }
 
     @Test
@@ -297,4 +345,205 @@ public class OrderProcessorTest {
         verify(mockOrderRepository, never()).save(any(Order.class));
     }
 
+    @Test
+    public void testFetchOrderCorrect() throws Exception {
+        Order order = new Order();
+        when(mockOrderRepository.existsById(1L)).thenReturn(true);
+        when(mockOrderRepository.getOne(1L)).thenReturn(order);
+
+        assertEquals(order, orderProcessor.fetchOrder(1L));
+        verify(mockOrderRepository, times(1)).getOne(1L);
+    }
+
+    @Test
+    public void testFetchOrderNonExistent() {
+        when(mockOrderRepository.existsById(1L)).thenReturn(false);
+        assertThrows(
+                Exception.class,
+                () -> orderProcessor.fetchOrder(1L), "Invalid order ID");
+        verify(mockOrderRepository, never()).getOne(1L);
+    }
+
+    @Test
+    public void testFetchOrderEmptyId() {
+        assertThrows(
+                Exception.class,
+                () -> orderProcessor.fetchOrder(null), "Invalid order ID");
+        verify(mockOrderRepository, never()).getOne(1L);
+    }
+
+    @Test
+    public void testFetchAllStoreOrdersAsRegional() throws Exception {
+        Order order1 = new Order();
+        order1.setStoreId(1L);
+        order1.setId(1L);
+        Order order2 = new Order();
+        order2.setStoreId(2L);
+        order2.setId(2L);
+        Order order3 = new Order();
+        order3.setStoreId(1L);
+        order3.setId(3L);
+        List<Order> orders = List.of(order1, order2, order3);
+
+        when(mockOrderRepository.findAll()).thenReturn(orders);
+        when(mockStoreCommunication.validateManager("man", "token")).thenReturn(false);
+
+        Collection<Order> res = orderProcessor.fetchAllStoreOrders("token", "man", "regional_manager", 1L);
+        assertThat(res).containsAll(List.of(order1, order3));
+
+    }
+
+    @Test
+    public void testFetchAllStoreOrdersAsStore() throws Exception {
+        Order order1 = new Order();
+        order1.setStoreId(1L);
+        order1.setId(1L);
+        Order order2 = new Order();
+        order2.setStoreId(2L);
+        order2.setId(2L);
+        Order order3 = new Order();
+        order3.setStoreId(1L);
+        order3.setId(3L);
+        List<Order> orders = List.of(order1, order2, order3);
+
+        when(mockOrderRepository.findAll()).thenReturn(orders);
+        when(mockStoreCommunication.validateManager("man", "token")).thenReturn(true);
+        when(mockStoreCommunication.getStoreIdFromManager("man", "token")).thenReturn(1L);
+
+        Collection<Order> res = orderProcessor.fetchAllStoreOrders("token", "man", "customer", 1L);
+        assertThat(res).containsAll(List.of(order1, order3));
+    }
+
+    @Test
+    public void testFetchAllStoreOrdersAsWrongStore() throws Exception {
+        Order order1 = new Order();
+        order1.setStoreId(1L);
+        order1.setId(1L);
+        Order order2 = new Order();
+        order2.setStoreId(2L);
+        order2.setId(2L);
+        Order order3 = new Order();
+        order3.setStoreId(1L);
+        order3.setId(3L);
+        List<Order> orders = List.of(order1, order2, order3);
+
+        when(mockOrderRepository.findAll()).thenReturn(orders);
+        when(mockStoreCommunication.validateManager("man", "token")).thenReturn(true);
+        when(mockStoreCommunication.getStoreIdFromManager("man", "token")).thenReturn(2L);
+
+        assertThrows(UnsupportedOperationException.class, () -> orderProcessor
+                .fetchAllStoreOrders(
+                        "token",
+                        "man",
+                        "customer",
+                        1L), "Not the store manager of this store");
+    }
+
+    @Test
+    public void testFetchAllStoreOrdersAsCustomer() {
+        Order order1 = new Order();
+        order1.setStoreId(1L);
+        order1.setId(1L);
+        Order order2 = new Order();
+        order2.setStoreId(2L);
+        order2.setId(2L);
+        Order order3 = new Order();
+        order3.setStoreId(1L);
+        order3.setId(3L);
+        List<Order> orders = List.of(order1, order2, order3);
+
+        when(mockOrderRepository.findAll()).thenReturn(orders);
+        when(mockStoreCommunication.validateManager("man", "token")).thenReturn(false);
+
+        assertThrows(Exception.class, () -> orderProcessor
+                .fetchAllStoreOrders("token", "man", "customer", 1L),
+                "Customers can not view store orders");
+    }
+
+    @Test
+    public void testFetchAllOrdersAsRegional() throws Exception {
+        Order order1 = new Order();
+        order1.setStoreId(1L);
+        order1.setId(1L);
+        Order order2 = new Order();
+        order2.setStoreId(2L);
+        order2.setId(2L);
+        Order order3 = new Order();
+        order3.setStoreId(1L);
+        order3.setId(3L);
+        List<Order> orders = List.of(order1, order2, order3);
+
+        when(mockOrderRepository.findAll()).thenReturn(orders);
+        assertThat(orderProcessor.fetchAllOrders("token", "deity", "regional_manager")).containsAll(orders);
+
+    }
+
+    @Test
+    public void testFetchAllOrdersAsCustomer() {
+        assertThrows(Exception.class, () -> orderProcessor
+                        .fetchAllOrders("token", "deity", "customer"),
+                "Only regional managers can view all orders");
+    }
+
+    @Test
+    public void testCancelOrderEmptyOrderId() {
+        assertThrows(IllegalArgumentException.class, () -> orderProcessor
+                .cancelOrder("token", "deity", "regional_manager", null),
+                "Invalid order ID");
+    }
+
+    @Test
+    public void testCancelOrderSuccessRegionalPlaced() throws Exception {
+        Order order = new Order();
+        order.setId(1L);
+        order.setStoreId(1L);
+        order.setStatus(Status.ORDER_PLACED);
+
+        Order order2 = new Order();
+        order2.setId(1L);
+        order2.setStoreId(1L);
+        order2.setStatus(Status.ORDER_CANCELED);
+
+        when(mockOrderRepository.getOne(1L)).thenReturn(order);
+        when(mockStoreCommunication.validateManager("deity", "token")).thenReturn(false);
+        orderProcessor.cancelOrder("token", "deity", "regional_manager", 1L);
+        verify(mockOrderRepository).save(order2);
+        verify(mockStoreCommunication).sendEmailToStore(1L, "Order with ID 1 cancelled", "token");
+    }
+
+    @Test
+    public void testCancelOrderSuccessRegionalOngoing() throws Exception {
+        Order order = new Order();
+        order.setId(1L);
+        order.setStoreId(1L);
+        order.setStatus(Status.ORDER_ONGOING);
+
+        Order order2 = new Order();
+        order2.setId(1L);
+        order2.setStoreId(1L);
+        order2.setStatus(Status.ORDER_CANCELED);
+
+        when(mockOrderRepository.getOne(1L)).thenReturn(order);
+        when(mockStoreCommunication.validateManager("deity", "token")).thenReturn(false);
+        orderProcessor.cancelOrder("token", "deity", "regional_manager", 1L);
+        verify(mockOrderRepository).save(order2);
+        verify(mockStoreCommunication).sendEmailToStore(1L, "Order with ID 1 cancelled", "token");
+    }
+
+    @Test
+    public void testCancelOrderExceptionRegional() {
+        Order order = new Order();
+        order.setId(1L);
+        order.setStoreId(1L);
+        order.setStatus(Status.ORDER_CANCELED);
+
+        when(mockOrderRepository.getOne(1L)).thenReturn(order);
+        when(mockStoreCommunication.validateManager("deity", "token")).thenReturn(false);
+        assertThrows(IllegalArgumentException.class, () -> orderProcessor
+                .cancelOrder("token", "deity", "regional_manager", 1L),
+                "No active order with this ID");
+
+        verify(mockOrderRepository, never()).save(any());
+        verify(mockStoreCommunication, never()).sendEmailToStore(anyLong(), anyString(), anyString());
+    }
 }
