0dc1adb188d3fee6955e3040654b7ca3f78a0952
Tom Kitak <t.kitak@student.tudelft.nl>
2022-12-23 17:41:56 +0100

    setLocation testing for edge cases and invalid input

--- order-microservice/src/test/java/nl/tudelft/sem/group06b/order/processor/OrderProcessorTest.java
+++ order-microservice/src/test/java/nl/tudelft/sem/group06b/order/processor/OrderProcessorTest.java
@@ -2,16 +2,16 @@ package nl.tudelft.sem.group06b.order.processor;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 import nl.tudelft.sem.group06b.order.communication.CouponCommunication;
 import nl.tudelft.sem.group06b.order.communication.MenuCommunication;
 import nl.tudelft.sem.group06b.order.communication.StoreCommunication;
 import nl.tudelft.sem.group06b.order.domain.Order;
+import nl.tudelft.sem.group06b.order.domain.Pizza;
 import nl.tudelft.sem.group06b.order.domain.Status;
 import nl.tudelft.sem.group06b.order.repository.OrderRepository;
 import nl.tudelft.sem.group06b.order.service.processor.OrderProcessor;
@@ -21,6 +21,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.scheduling.TaskScheduler;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootTest
 public class OrderProcessorTest {
@@ -96,7 +102,8 @@ public class OrderProcessorTest {
         Long orderId = 1L;
         assertThrows(
                 IllegalArgumentException.class,
-                () -> orderProcessor.setOrderTime(orderId, ""), "Invalid time. The time should be at least 30 minutes after the current time");
+                () -> orderProcessor.setOrderTime(orderId, ""),
+                "Invalid time. The time should be at least 30 minutes after the current time");
         verify(mockOrderRepository, never()).save(any(Order.class));
     }
 
@@ -104,8 +111,8 @@ public class OrderProcessorTest {
     public void testSetOrderTimeInvalidStatus() throws Exception {
         Order order = new Order();
 
-        Long orderId = 1L;
-        String time = "20/12/2022 16:11:30";
+        final Long orderId = 1L;
+        final String time = "20/12/2022 16:11:30";
 
         order.setStatus(Status.ORDER_PLACED);
         order.setId(orderId);
@@ -130,6 +137,7 @@ public class OrderProcessorTest {
         order.setStatus(Status.ORDER_ONGOING);
 
         when(mockStoreCommunication.getStoreIdFromLocation(location, "token")).thenReturn(storeId);
+        when(mockStoreCommunication.validateLocation(location, "token")).thenReturn(true);
         when(mockOrderRepository.getOne(orderId)).thenReturn(order);
 
         orderProcessor.setOrderLocation("token", orderId, location);
@@ -140,7 +148,53 @@ public class OrderProcessorTest {
         targetOrder.setStatus(Status.ORDER_ONGOING);
         targetOrder.setStoreId(storeId);
 
-        verify(mockOrderRepository, times(1)).save(targetOrder);
+        verify(mockOrderRepository, times(1)).save(any(Order.class));
+    }
+
+    @Test
+    public void testSetOrderLocationInvalidEmptyLocationException() throws Exception {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> orderProcessor.setOrderLocation("token", 1L, ""), "Invalid store location");
+        verify(mockOrderRepository, never()).save(any(Order.class));
+    }
+
+    @Test
+    public void testSetOrderLocationInvalidLocationException() throws Exception {
+
+        when(mockStoreCommunication.validateLocation("not valid location", "token")).thenReturn(false);
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> orderProcessor.setOrderLocation("token", 1L, "not valid location"), "Invalid store location");
+        verify(mockOrderRepository, never()).save(any(Order.class));
+    }
+
+    @Test
+    public void testSetOrderLocationInvalidOrderStage() throws Exception {
+        Order order = new Order();
+
+        String location = "location";
+        Long id = 1L;
+
+        order.setId(id);
+        order.setStatus(Status.ORDER_FINISHED);
+
+        when(mockStoreCommunication.validateLocation(location, "token")).thenReturn(true);
+        when(mockStoreCommunication.getStoreIdFromLocation(location, "token")).thenReturn(10L);
+        when(mockOrderRepository.getOne(id)).thenReturn(order);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> orderProcessor.setOrderLocation("token", id, location), "No active order with this ID");
+        verify(mockOrderRepository, never()).save(any(Order.class));
+    }
+
+    @Test
+    public void testPlaceOrderInvalidOrderId() throws Exception {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> orderProcessor.placeOrder("token", null), "Invalid order ID");
+        verify(mockOrderRepository, never()).save(any(Order.class));
     }
 
 
