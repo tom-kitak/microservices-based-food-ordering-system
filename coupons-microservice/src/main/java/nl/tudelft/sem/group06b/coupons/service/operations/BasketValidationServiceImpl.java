ad2408e75745f4a6dfd59ec05ff04adc460a11ad
Catalin Brita <c.e.brita@student.tudelft.nl>
2023-01-17 19:52:55 +0100

    Added javadoc to interface class

--- coupons-microservice/src/main/java/nl/tudelft/sem/group06b/coupons/service/operations/BasketValidationServiceImpl.java
+++ coupons-microservice/src/main/java/nl/tudelft/sem/group06b/coupons/service/operations/BasketValidationServiceImpl.java
@@ -1,19 +1,25 @@
 package nl.tudelft.sem.group06b.coupons.service.operations;
 
+import java.math.BigDecimal;
+import java.util.List;
+import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import nl.tudelft.sem.group06b.coupons.model.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.math.BigDecimal;
-import java.util.List;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Transactional
 public class BasketValidationServiceImpl implements BasketValidationService {
 
+    /**
+     * Given a basket, decided what is the optimal choice.
+     *
+     * @param pizzasWithDiscount the pizzas that can be applied a discount coupon
+     * @param pizzasWithOneOff the pizzas that can be applied a oneOffCoupon
+     * @return which type of coupon should the user apply
+     */
     public int minimumBasket(List<Pizza> pizzasWithDiscount, List<Pizza> pizzasWithOneOff) {
         BigDecimal discountPrice = pizzasWithDiscount.stream().map(Pizza::getPrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
@@ -25,12 +31,22 @@ public class BasketValidationServiceImpl implements BasketValidationService {
         return 1;
     }
 
+    /**
+     * Throws an exception in case the basket is empty.
+     *
+     * @param emptyBasket whether the basket is empty or not
+     */
     public void throwEmptyBasket(boolean emptyBasket) {
         if (emptyBasket) {
             throw new IllegalArgumentException("The basket is empty");
         }
     }
 
+    /**
+     * Throws an exception in case there are no coupons.
+     *
+     * @param noCoupons whether there are coupons to apply or not
+     */
     public void throwNoCoupons(boolean noCoupons) {
         if (noCoupons) {
             throw new IllegalArgumentException("No coupons found");
