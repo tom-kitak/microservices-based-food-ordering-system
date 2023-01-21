d1d11f32215722ed8a2d8c0f7fc1441f2e732ce3
Tezzish <ishanpahwa14@gmail.com>
2023-01-21 02:26:56 +0100

    Checkstyle fix

--- order-microservice/src/main/java/nl/tudelft/sem/group06b/order/communication/MenuCommunication.java
+++ order-microservice/src/main/java/nl/tudelft/sem/group06b/order/communication/MenuCommunication.java
@@ -23,6 +23,7 @@ public class MenuCommunication {
     public MenuCommunication() {
         this.restTemplate = new RestTemplate();
     }
+
     public MenuCommunication(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
